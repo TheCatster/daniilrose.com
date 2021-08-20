d64fb9e6edb2107c9ab7c3205644822e3c19a232
Daniel Rose <danielrose@member.fsf.org>
2021-08-20 06:34:45 -0400

    Add hyperlink for site name

--- components/Navbar.js
+++ components/Navbar.js
@@ -12,7 +12,13 @@ export default function Navbar({ children }) {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16">
             <div className="flex items-center">
-              <div className="flex-shrink-0 font-bold text-xl">Daniel Rose</div>
+              <Link href="/">
+                <a>
+                  <div className="flex-shrink-0 font-bold text-xl">
+                    Daniel Rose
+                  </div>
+                </a>
+              </Link>
               <div className="hidden md:block">
                 <div className="ml-10 flex items-baseline space-x-4">
                   <Link href="/">
