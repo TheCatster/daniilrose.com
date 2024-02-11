2534f071d2a545736bc5c690995d39c0e7038375
Daniil Rose <daniil.rose@posteo.org>
2024-02-11 16:59:13 -0500

    Update Script location

--- pages/index.js
+++ pages/index.js
@@ -5,19 +5,19 @@ import Intro from "../components/aboutme/Intro";
 import Interests from "../components/aboutme/Interests";
 
 export default function Home() {
-  return (
-    <Layout>
-      <Head>
-        <title>Daniil Rose</title>
-        <Script
-          async
-          defer
-          data-website-id="92fdacb6-2085-4d3d-b97c-8e0764149428"
-          src="https://webstats.daniilrose.com/umami.js"
-        />
-      </Head>
-      <Intro />
-      <Interests />
-    </Layout>
-  );
+    return (
+        <Layout>
+            <Script
+                async
+                defer
+                data-website-id="92fdacb6-2085-4d3d-b97c-8e0764149428"
+                src="https://webstats.daniilrose.com/umami.js"
+            />
+            <Head>
+                <title>Daniil Rose</title>
+            </Head>
+            <Intro />
+            <Interests />
+        </Layout>
+    );
 }
